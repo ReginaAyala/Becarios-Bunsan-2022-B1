5a81a3b154a053f298476839dcf83503a1fe7e41
ReginaAyala <regis3096@gmail.com>
2022-05-09 08:42:11 -0500

    Calculator Agent

---
+++ lib/calculator_agent.ex
@@ -0,0 +1,25 @@
+defmodule CalculatorAgent do
+    def init(init_val) do
+        {_, pid} = Agent.start(fn -> init_val end)
+        pid
+    end
+
+    def sum(pid, val),  do:  calc({:sum, val, pid})
+    def rest(pid, val),  do:  calc({:rest, val, pid})
+    def mult(pid, val), do:  calc({:mult, val, pid})
+    def div(pid, val),  do:  calc({:div, val, pid})
+    def state(pid),     do:  calc({:state, pid})
+
+    defp calc(operation) do
+        case operation do
+
+            {:sum, val, pid}   -> Agent.update(pid, &(&1 + val)) 
+            {:rest, val, pid}  -> Agent.update(pid, &(&1 - val)) 
+            {:mult, val, pid}  -> Agent.update(pid, &(&1 * val)) 
+            {:div, val, pid}   -> Agent.update(pid, &(&1 / val)) 
+            {:state, pid}      -> Agent.get(pid, &(&1))
+    
+          _ ->  IO.puts("Invalid request")
+        end
+    end   
+end
